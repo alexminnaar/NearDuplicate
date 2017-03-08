28b8b290581c85712013a917b742165dd20aab58
alexminnaar <Bailey12>
2017-03-07 16:10:29 -0800

    added sqs polling

--- DuplicateClusterAssignment.py
+++ DuplicateClusterAssignment.py
@@ -1,44 +1,56 @@
-from image_match.elasticsearch_driver import SignatureES
-from elasticsearch import Elasticsearch
 import uuid
 from collections import defaultdict
 import logging
+import hashlib
+
+from image_match.elasticsearch_driver import SignatureES
+from elasticsearch import Elasticsearch
+from pymemcache.client.base import Client
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 class DuplicateClusterAssignment:
-    def __init__(self, es_index, distance_threshold):
+    def __init__(self, es_index, distance_threshold, memcached_endpoint):
         self.es_client = Elasticsearch()
         self.ses = SignatureES(self.es_client, index=es_index, distance_cutoff=distance_threshold)
+        self.memcached_client = Client((memcached_endpoint, 11211))
 
-    # def image_url_exists(self, image_url):
-    #     '''
-    #     check if image url already exists in elasticsearch.  Image url's are stored in the 'path' field. Returns boolean.
-    #     :param image_url: Query image url.
-    #     :return: Boolean (True if it exists, False if it doesn't).
-    #     '''
-    #
-    #     query = {
-    #         "query": {
-    #             "match": {
-    #                 "path": image_url
-    #             }
-    #         }
-    #     }
-    #
-    #     res = self.es_client.search(index="images", body=query)
-    #
-    #     # if there are results then this image url already exits in ES
-    #     if len(res['hits']['hits']) > 0:
-    #         logger.info("Image URL: %s found in elasticsearch" % image_url)
-    #         for hit in res['hits']['hits']:
-    #             print hit['_source']['path']
-    #         return True
-    #     else:
-    #         logger.info("Image URL: %s not found in elasticsearch" % image_url)
-    #         return False
+    def image_url_exists(self, image_url):
+        '''
+        check if image url already exists in elasticsearch.  Image url's are stored in the 'path' field. Returns boolean.
+        :param image_url: Query image url.
+        :return: Boolean (True if it exists, False if it doesn't).
+        '''
+
+        image_url_query = {
+            "query": {
+                "match": {
+                    "path": image_url
+                }
+            }
+        }
+
+        # not an extact match query but if there is an exact match it should be the first result, so manually check if
+        # the first result is equivalent to the query image url
+        res = self.es_client.search(index="images", body=image_url_query)
+        hits = res['hits']['hits']
+
+        if len(hits) > 0:
+            top_res = hits[0]['_source']['path']
+
+            # if there are results then this image url already exits in ES
+            if top_res == image_url:
+                logger.info("Image URL: %s found in elasticsearch" % image_url)
+                return True
+            else:
+                logger.info("Image URL: %s not found in elasticsearch" % image_url)
+                return False
+        else:
+            logger.info("Image URL: %s not found in elasticsearch" % image_url)
+            return False
 
     def get_near_duplicates(self, image_url):
         '''
@@ -88,6 +100,12 @@ class DuplicateClusterAssignment:
             logger.info("No near-duplicates so assigning random cluster id")
             return random_cluster_id
 
+
+    def memcached_insert(self, key, value):
+
+        self.memcached_client.set('%s' % hashlib.md5(key).hexdigest(), str(value))
+
+
     def insert_and_cluster(self, image_url):
         '''
         Given an image url,
@@ -98,42 +116,28 @@ class DuplicateClusterAssignment:
         :param image_url: Image url to be clustered and indexed.
         '''
 
-        try:
-            near_dups = self.get_near_duplicates(image_url)
-            cluster_id = self.get_cluster_id(near_dups)
-            self.index_image_with_clusterid(image_url, image_clusterid=cluster_id)
-        except Exception:
-            logger.error("Indexing pipeline failure", exc_info=True)
+        if not self.image_url_exists(image_url):
+
+            try:
+                near_dups = self.get_near_duplicates(image_url)
+                cluster_id = self.get_cluster_id(near_dups)
+                self.index_image_with_clusterid(image_url, image_clusterid=cluster_id)
+                self.memcached_insert(image_url,cluster_id)
+            except Exception:
+                logger.error("Indexing pipeline failure", exc_info=True)
+        else:
+            logger.info("Exiting because image already exists in elasticsearch")
 
 
 def main():
-    # es = Elasticsearch()
+    es = Elasticsearch()
     # es.indices.delete(index='images', ignore=[400, 404])
     # es.indices.create(index='images')
 
-    dca = DuplicateClusterAssignment(es_index='images', distance_threshold=0.3)
+    dca = DuplicateClusterAssignment(es_index='images', distance_threshold=0.3, memcached_endpoint='localhost')
 
-    dca.insert_and_cluster(image_url="https://pmcfootwearnews.files.wordpress.com/2016/12/dogs-in-shoes-yeezy-sneakers.jpg?w=1024")
-
-
-    #
-    # counter = 0
-    #
-    # with open('/users/alexminnaar/image_urls__vig_cats.txt', 'r') as f:
-    #     for lines in f:
-    #         counter += 1
-    #         image_url = lines.split('|')[0]
-    #         dca.insert_and_cluster(image_url)
-    #
-    #         if counter % 10 == 0:
-    #             print counter
-
-    # res = es.search(index="images", body={"query": {"match_all": {}}})
-    # for hit in res['hits']['hits']:
-    #     print hit['_source']['path']
-    #
-    # exists = dca.image_url_exists("blah")
-    # print exists
+    dca.insert_and_cluster(
+        image_url="https://images.viglink.com/product/250x250/images-footaction-com/20d7125041a8c808e798e933741511cb66f3a7ee.jpg?url=http%3A%2F%2Fimages.footaction.com%2Fis%2FBA8920%2Flarge_wide%2Fadidas-ultra-boost-mens%2F")
 
 
 if __name__ == "__main__":
---
+++ SQSPolling.py
@@ -0,0 +1,36 @@
+import logging
+
+import boto3
+
+from DuplicateClusterAssignment import DuplicateClusterAssignment
+
+
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger(__name__)
+
+
+def sqs_polling(queue_name, memcache_endpoint):
+    # SQS client config
+    sqs = boto3.resource('sqs', region_name='us-east-1')
+    queue = sqs.get_queue_by_name(QueueName=queue_name)
+
+    dca = DuplicateClusterAssignment(es_index='images', distance_threshold=0.3, memcached_endpoint='localhost')
+
+    # poll sqs forever
+    while 1:
+        # receives up to 10 messages at a time
+        for message in queue.receive_messages():
+            logger.warning("Read message: %s" % message.body)
+
+            image_url = message.body
+
+            # cluster image and insert to memcached
+            dca.insert_and_cluster(image_url)
+
+
+def main():
+    sqs_polling('image-similarity-qa', 'localhost')
+
+
+if __name__ == "__main__":
+    main()
